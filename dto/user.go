dcc8b26563fee3eec2d8eb641eb9a48cd39c8809
Conrad Bekondo <conradbekondo@gmail.com>
2024-11-24 17:59:46 +0100

    chore: deploy sample webhook

--- dto/user.go
+++ dto/user.go
@@ -148,9 +148,9 @@ func (n NewUserRequest) Validate() error {
 		msgs = append(msgs, err.Error())
 	}
 
-	if len(n.Phone) > 0 && !regexp.MustCompile(`\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$`).MatchString(n.Phone) {
-		msgs = append(msgs, "Invalid phone number. Phone numbers must be in international format")
-	}
+	// if len(n.Phone) > 0 && !regexp.MustCompile(`\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$`).MatchString(n.Phone) {
+	// 	msgs = append(msgs, "Invalid phone number. Phone numbers must be in international format")
+	// }
 
 	if len(n.CaptchaToken) == 0 {
 		msgs = append(msgs, "Invalid captcha token")
