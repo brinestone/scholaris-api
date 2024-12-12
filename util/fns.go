55f3c3c338d9528ec6fe12f55241b06626880fbc
Conrad Bekondo <conradbekondo@gmail.com>
2024-12-12 10:41:10 +0100

    fix(permissions): fix listRelations endpoint caching key gen 2

--- util/fns.go
+++ util/fns.go
@@ -3,19 +3,13 @@ package util
 import (
 	"crypto/md5"
 	"encoding/hex"
-
-	"encore.dev/rlog"
 )
 
 func HashThese(args ...string) string {
 	hash := md5.New()
 	for _, arg := range args {
-		rlog.Debug("hash", "results", hash.Sum([]byte(arg)))
+		hash.Write([]byte(arg))
 	}
-	buf := make([]byte, hash.Size())
-	rlog.Debug("bytes", "bytes", string(buf))
-	hash.Write(buf)
-	rlog.Debug("bytes", "bytes", string(buf))
 
-	return hex.EncodeToString(buf)
+	return hex.EncodeToString(hash.Sum(nil))
 }
